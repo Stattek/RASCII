906deaccac7a9040e0ff0f5c78dbc7bc799b97cc
stattek <alienhuntsgames@gmail.com>
2025-02-17 00:14:32 -0600

    Add enums for character sets to make it easier to work with when multithreading.

--- src/charsets.rs
+++ src/charsets.rs
@@ -29,6 +29,45 @@ pub const MINIMAL: &[&str] = &[
     " ", " ", ".", ":", "'", ";", "o", "l", "d", "c", "x", "0", "k", "K", "N", "M", "W", "X",
 ];
 
+/// Represents all Charsets that exist in this program.
+pub enum Charset {
+    Block,
+    Chinese,
+    Default,
+    Emoji,
+    Russian,
+    Slight,
+    Minimal,
+}
+
+/// Get a `Charset` from an enum.
+pub fn from_enum(charset: Charset) -> &'static [&'static str] {
+    match charset {
+        Charset::Block => BLOCK,
+        Charset::Chinese => CHINESE,
+        Charset::Default => DEFAULT,
+        Charset::Emoji => EMOJI,
+        Charset::Russian => RUSSIAN,
+        Charset::Slight => SLIGHT,
+        Charset::Minimal => MINIMAL,
+    }
+}
+
+/// Converts a string into a `Charset`.
+pub fn to_charset_enum(s: &str) -> Option<Charset> {
+    match s {
+        "block" => Some(Charset::Block),
+        "chinese" => Some(Charset::Chinese),
+        "default" => Some(Charset::Default),
+        "emoji" => Some(Charset::Emoji),
+        "russian" => Some(Charset::Russian),
+        "slight" => Some(Charset::Slight),
+        "minimal" => Some(Charset::Minimal),
+        _ => None,
+    }
+}
+
+/// Get a charset from a string slice.
 pub fn from_str(s: &str) -> Option<&[&str]> {
     match s {
         "block" => Some(BLOCK),
